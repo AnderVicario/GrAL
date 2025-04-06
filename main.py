4e117efdceda7ac82522d1e5c873f0b16dfae3cb
AnderVicario <andervicariozabala@gmail.com>
2025-04-06 13:50:01 +0200

    fix: inconsistent main

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ def main(prompt, advanced_mode=False):
     # prompt = "I want to invest in Tesla and Bitcoin. What can you tell me about them?"
     agent = SearchAgent(user_prompt=prompt)
     full_report = agent.process_all(advanced_mode)
-    
-    print("Reporte Consolidado:")
     print(full_report)
     return full_report
 
